4a8c8c77842befae0ae75255d637fbc08e6fa4ca
Ngoc Pham <tuanngocptn@users.noreply.github.com>
2017-10-31 22:09:34 +0700

    Update Main.java
    edit

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-class HelloWorldApp {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Prints the string to the console.
+import javax.swing.*;
+
+public class Hello extends JFrame {
+    public Hello() {
+        super("hello");
+        super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        super.add(new JLabel("Hello, world!"));
+        super.pack();
+        super.setVisible(true);
+    }
+
+    public static void main(final String[] args) {
+        new Hello();
     }
 }
